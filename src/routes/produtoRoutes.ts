b5924145444758b3cdf72f39f89f65b398ddfbd7
Patricia Nunes Ferreira <pferreira@firjan.com.br>
2026-04-01 21:45:45 -0300

    adiciona o crud completo e produtos

--- src/routes/produtoRoutes.ts
+++ src/routes/produtoRoutes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { criarProduto, getProdutos } from "../controllers/produtoController.js";
+import { atualizarProduto, criarProduto, deletarProduto, getProdutoPorId, getProdutos } from "../controllers/produtoController.js";
 
 const router = Router();
 
 router.get("/", getProdutos);
+router.get("/:id", getProdutoPorId);
 router.post("/", criarProduto);
+router.patch("/:id", atualizarProduto);
+router.delete("/:id", deletarProduto)
 
 export const produtoRoutes = router;
