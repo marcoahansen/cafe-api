0888d86911ac2ba6c920ce52687cb28e17afcbce
Patricia Nunes Ferreira <pferreira@firjan.com.br>
2026-04-06 20:32:37 -0300

    implementa listar todos pedidos

--- src/controllers/pedidoController.ts
+++ src/controllers/pedidoController.ts
@@ -1,6 +1,20 @@
 import type { Request, Response } from "express";
 import { PedidoModel, type INovoItemPedido } from "../models/Pedido.js";
 
+export const getPedidos = async (req: Request, res: Response) => {
+  try {
+    const pedidos = await PedidoModel.listarTodos();
+    return res.json(pedidos);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return res.status(400).json({ error: error.message });
+    }
+    return res
+      .status(500)
+      .json({ error: "Ocorreu um erro inesperado ao listar os pedidos." });
+  }
+};
+
 export const postPedido = async (req: Request, res: Response) => {
   const itens: INovoItemPedido[] = req.body.itens;
   if (!itens || !Array.isArray(itens)) {
